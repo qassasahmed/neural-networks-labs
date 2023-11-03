f573a612f03fffa6d301166aff8976971592e628
qassasahmed <qassas.ahmed@mau.edu.eg>
2023-11-03 14:14:27 +0200

    lab-3 [beta 2] with GUI

--- lab-3/main.py
+++ lab-3/main.py
@@ -1,10 +1,61 @@
+import pandas as pd
 import perc as p
+import tkinter as tk
+from tkinter import ttk, messagebox
+import tkinter.filedialog as fd
+import ctypes
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
 
 if __name__ == '__main__':
-    x = [[1, 1, 1, 1, 1],
-         [-1, 1, -1, -1, 1],
-         [1, 1, 1, -1, 1],
-         [1, -1, -1, 1, 1]]
 
-    per = p.Perceptron(5)
-    print(per.run(x, [1, 1, -1, -1]))
+    def show_res():
+        x, t = read_file()
+        pre = p.Perceptron(len(x[0]), 1)
+        epochs = pre.run(x, t)
+        labels = []
+        for i in range(len(epochs)):
+            labels.append(ttk.Label(frame,
+                                    text=f"Epoch {i + 1}: {[int(c) for c in epochs[i]]}",
+                                    style="TLabel"))
+            labels[i].pack(padx=10, pady=10)
+        labels.append(ttk.Label(frame,
+                                text=f"Operation Complete!",
+                                style="TLabel"))
+        labels[-1].pack(padx=10, pady=10)
+
+
+    def read_file():
+        try:
+            file_path = fd.askopenfilename()
+            df = pd.read_csv(file_path)
+            target = df.iloc[:, -1].values.tolist()
+            features = df.iloc[:, :-1].values.tolist()
+            return features, target
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred: {e}")
+
+    root = tk.Tk()
+    root.title("Perceptron")
+    root.geometry("500x500")
+    # root.resizable(False, False)
+    frame = tk.Frame(root)
+    frame.pack()
+
+    style_label = ttk.Style()
+    style_label.configure("TLabel",
+                          font=("Sans Serif", 15),
+                          padding=10)
+
+    style_btn = ttk.Style()
+    style_btn.configure("TButton",
+                        font=("Sans Serif", 15),
+                        padding=10)
+
+    btn_run = ttk.Button(frame,
+                         text="Run",
+                         command=show_res,
+                         style="TButton")
+    btn_run.pack(padx=10, pady=10)
+
+    root.mainloop()
--- lab-3/perc.py
+++ lab-3/perc.py
@@ -2,52 +2,47 @@ import numpy as np
 
 
 def activation_function(y_in):
+    """
+    Threshold Activation function with Theta equals zero.
+    :param y_in: the linear summation of wi * xi
+    :return:  0 if y_in == 0, 1 if y_in > 0, -1 if y_in <0
+    """
+    print(f"y_in:: {y_in : <10}  y_out:: {0 if y_in == 0 else y_in / abs(y_in)}")
     return 0 if y_in == 0 else y_in / abs(y_in)
 
 
 def y_o(inputs, weights):
     """
-    y_in = sum(wi * xi)
-    :return: activation_function(sum)
+    Calculates the linear summation of wi * xi, then calls the activation function
+    :param inputs: list of xi
+    :param weights: list of wi
+    :return: y_o which the non-linear threshold activation of the linear summation
     """
-    sum = 0
+    summation = 0
     for i in range(len(inputs)):
-        sum += inputs[i] * weights[i]
-    return activation_function(sum)
+        summation += inputs[i] * weights[i]
+    return activation_function(summation)
 
 
 def update_weights(w, d):
     """
-    w new = w old + delta
-    :return: list of new weights
+
+    :param w:
+    :param d:
+    :return:
     """
     return [w[i] + d[i] for i in range(len(w))]
 
 
 class Perceptron:
 
-    def __init__(self, number_of_features):
+    def __init__(self, number_of_features, eta):
         self.old_weights = list(np.zeros(number_of_features))
-        self.no_update = list(np.zeros(number_of_features))
-        self.eta = 1
+        self.no_delta_update = list(np.zeros(number_of_features))
         self.deltas = []
-
-    def run(self, matrix, target):
-        """
-        1] y_o(row, initial weights)
-        2] check y_o == target ->>>> flag
-        3] delta_values()
-        4] new weights
-        5] old_wights = new weights
-        6] repeat
-        :return: epoch weights
-        """
-        i = 0
-        for row in matrix:
-            d = self.delta_values(y_o(row, self.old_weights, ) == target[i], row, target[i])
-            self.old_weights = update_weights(self.old_weights, d)
-            i = i + 1
-        return self.old_weights
+        self.eta = eta
+        self.current_epoch = []
+        self.all_epochs = []
 
     def delta_values(self, flag, row, T):
         """
@@ -55,7 +50,31 @@ class Perceptron:
         :return: list of deltas
         """
         if flag:
-            return self.no_update
+            return self.no_delta_update
         else:
             self.deltas = [i * self.eta * T for i in row]
             return self.deltas
+
+    def run(self, matrix, target):
+        for epoch in range(5):
+            i = 0
+            for row in matrix:
+                y_output = y_o(row, self.old_weights)
+                delta = self.delta_values(y_output == target[i], row, target[i])
+                self.old_weights = update_weights(self.old_weights, delta)
+                i = i + 1
+            print(f"epoch {epoch + 1}: {self.old_weights} \n")
+            if self.old_weights == self.current_epoch:
+                self.all_epochs.append(self.old_weights)
+                print("Right weights were found")
+                break
+            else:
+                # save the weights of current epoch to compare it with next epoch
+                self.all_epochs.append(self.old_weights)
+                self.current_epoch = self.old_weights
+
+        return self.all_epochs
+
+
+if __name__ == '__main__':
+    print("go to main")
